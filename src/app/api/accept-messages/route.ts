a0b4977737dd5bd28c505ca11032fb23e96d1aaa
Mohit Thapa <thapamohit2058@gmail.com>
2025-10-04 22:31:42 +0545

    added accept message route for accepting messages

---
+++ src/app/api/accept-messages/route.ts
@@ -0,0 +1,68 @@
+import { auth } from "@/auth";
+import dbConnect from "@/lib/dbConnect";
+import { User, UserModel } from "@/model/user";
+
+export async function POST(request: Request) {
+  await dbConnect();
+
+  const session = await auth();
+  const user: User = session?.user;
+
+  if (!session || !session.user) {
+    return Response.json(
+      {
+        success: false,
+        message: "not authenticated",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+
+  const userId = await user._id;
+
+  const { acceptMessages } = await request.json();
+
+  try {
+    const updatedUser = await UserModel.findByIdAndUpdate(
+      userId,
+      { isAcceptingMessage: acceptMessages },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return Response.json(
+        {
+          success: false,
+          message: "failed to update user status for accepting messages",
+        },
+        {
+          status: 401,
+        }
+      );
+    } else {
+      return Response.json(
+        {
+          success: true,
+          message: "message status updated successfully",
+        },
+        {
+          status: 200,
+        }
+      );
+    }
+  } catch (error) {
+    console.log("failed to updated user status for accepting messages", error);
+
+    return Response.json(
+      {
+        success: false,
+        message: "failed to updated user status for accepting messages",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+}
